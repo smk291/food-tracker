e7d3f29e1ff38a0dd8509cd70f322bebdf9c5abe
mikeechen <mikeechen915@gmail.com>
2016-11-30 14:42:47 -0800

    side nav is done

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router';
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.navTransition = this.navTransition.bind(this);
+  }
+
+  navTransition(e) {
+    e.preventDefault();
+    const sideNav = ReactDOM.findDOMNode(this.refs.sideNav);
+    if (e.value === 'close') {
+      sideNav.classList.remove("show-nev");
+    } else {
+      sideNav.classList.toggle("show-nav");
+    }
+  }
+
   render() {
     return (
       <header>
-        <div className="navbar-fixed">
-          <nav>
-            <div className="nav-wrapper">
-              <ul className="right">
-                <li><a href="">Profile Home</a></li>
-                <li><a href="">Log Out</a></li>
-              </ul>
+          <nav className="navigation">
+            <a href="" onClick={this.navTransition}><i className="material-icons invoke">menu</i></a>
+            <div ref="sideNav" className="side-nav">
+              <a href="" className="closebtn" onClick={this.navTransition}><i className="material-icons">close</i></a>
+              <a href="#">Profile</a>
+              <a href="#">Search Meals</a>
+              <a href="#">Review Meals</a>
+              <a href="#">Analyze</a>
+              <a href="#">Log Out</a>
             </div>
           </nav>
-        </div>
       </header>
     );
   }
--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from './Header'
 import Footer from './Footer';
 
 export default class Profile extends React.Component {
   render() {
     return (
       <div>
-        Profile
+        <Header />
+        {/* <Body /> */}
       </div>
     );
   }

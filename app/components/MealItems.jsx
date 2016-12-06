738c68a48bf9f6b5e06127ecea22d537f489f818
mikeechen <mikeechen915@gmail.com>
2016-12-05 20:42:08 -0800

    a work around..

--- app/components/MealItems.jsx
+++ app/components/MealItems.jsx
@@ -9,27 +9,37 @@ export default class MealItems extends React.Component {
 
   render() {
     let mealArr = [];
-    const dayArr = [
-     'Sunday',
-     'Monday',
-     'Tuesday',
-     'Wednesday',
-     'Thursday',
-     'Friday',
-     'Saturday'
-   ];
+    const dayObj = {
+      Sun: 'Sunday',
+      Mon: 'Monday',
+      Tue: 'Tuesday',
+      Wed: 'Wednesday',
+      Thu: 'Thursday',
+      Fri: 'Friday',
+      Sat: 'Saturday'
+    };
+    const monthObj = {
+      Jan: 1,
+      Feb: 2,
+      Mar: 3,
+      Apr: 4,
+      May: 5,
+      Jun: 6,
+      Jul: 7,
+      Aug: 8,
+      Sep: 9,
+      Oct: 10,
+      Nov: 11,
+      Dec: 12
+    };
 
     _.forEach(this.props.groupByDay, (elm, key) => {
-      console.log(key);
-      const newDate = new Date(key.toString().substring(0, key.indexOf('T')));
-      // .toUTCString();
-      // const altDate = newDate.split(' ');
-      // console.log(altDate);
-      console.log(newDate);
-      const year = newDate.getFullYear();
-      const month = newDate.getMonth() + 1;
-      const date = newDate.getDate();
-      const day = dayArr[newDate.getDay()];
+      const newDate = new Date(key).toUTCString();
+      const altDate = newDate.split(' ');
+      const year = altDate[3];
+      const month = monthObj[altDate[2]];
+      const date = altDate[1];
+      const day = dayObj[altDate[0].substring(0, altDate[0].indexOf(','))];
       mealArr.push(<MealItem
         key={key}
         day={day}
